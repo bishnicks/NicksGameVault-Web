430ae8d21eabe7ca26996b735b906f570c7f6fd5
Nick <nbishop7132@gmail.com>
2026-08-22 16:17:55 -0400

    Apply portrait layout without pointer detection

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nicks-game-vault-v3';
+const CACHE='nicks-game-vault-v4';
 const CORE=['./arcade-launcher.html','./neon-creek-dash.html','./star-swarm.html','./wildscale.html','./hummingbird-haven.html','./Starry-icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
